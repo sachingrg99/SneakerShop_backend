483579351132cc52672853f7fa24c6dc325f1faa
sachingrg99 <sachingurung253@gmail.com>
2022-08-03 19:29:22 +0545

    Finalizing Sneaker store

---
+++ routes/users.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { signin, signup, deleteUsers, updateUsers, getUsers, getVerified, getMessage } from '../controller/user.js';
+import auth from '../middleware/auth.js';
+const router = express.Router();
+
+router.post('/signin', signin)
+router.post('/signup', signup)
+router.get('/all', auth, getUsers)
+router.post('/play', getMessage)
+router.get('/:id/verify/:token', getVerified)
+router.patch('/:id', auth, updateUsers)
+router.delete('/:id', auth, deleteUsers)
+export default router;
